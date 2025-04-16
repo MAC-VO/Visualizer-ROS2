fc6af8dd85d9dfe8df4dd47d809662319e83c675
haleqiu <yuhengq@andrew.cmu.edu>
2025-04-16 14:52:12 -0400

    for compressed image

--- macvo_vis/macvo_vis.py
+++ macvo_vis/macvo_vis.py
@@ -1,39 +1,44 @@
 import rclpy
 from packaging import version
+import numpy as np
 
 import rerun as rr
 assert version.parse(rr.__version__) >= version.parse("0.22.0")
 
 from rclpy.node import Node
 from rclpy.time import Time
-from sensor_msgs.msg import Image, PointCloud
+from sensor_msgs.msg import Image, PointCloud, CompressedImage
 from geometry_msgs.msg import PoseStamped
 from message_filters import ApproximateTimeSynchronizer, Subscriber
 
-from .MessageFactory import from_stamped_pose, from_image, from_pointcloud
+from .MessageFactory import from_stamped_pose, from_image, from_pointcloud, from_compressed_image
 
 
 class MACVO_visualizer_Node(Node):
     NODENAME = "macvo_visualizer"
     TIMELINE = "ros_time_ns"
     
-    def __init__(self, image_topic: tuple[str, str] | None, pose_topic: str, map_topic: str | None = None):
+    def __init__(self, imageL_topic: str | None, imageR_topic: str | None, pose_topic: str, map_topic: str | None = None):
         super().__init__(self.NODENAME)
         rr.init(self.NODENAME, spawn=True)
         rr.set_time_sequence(self.TIMELINE, 0)
         rr.log("/log", rr.TextLog("Rerun session initialized"))
         rr.log("/", rr.ViewCoordinates(xyz=rr.ViewCoordinates.FRD), static=True)
         
-        if image_topic is not None:
-            imageL_topic: str; imageR_topic: str
-            imageL_topic, imageR_topic = image_topic
+        self.sync_stereo = None
 
-            self.imageL_sub = Subscriber(self, Image, imageL_topic, qos_profile=1)
-            self.imageR_sub = Subscriber(self, Image, imageR_topic, qos_profile=1)
+        if imageL_topic is not None and imageR_topic is not None:
+            self.imageL_sub = Subscriber(self, CompressedImage, imageL_topic + "/compressed", qos_profile=1)
+            self.imageR_sub = Subscriber(self, CompressedImage, imageR_topic + "/compressed", qos_profile=1)
             self.sync_stereo = ApproximateTimeSynchronizer(
                 [self.imageL_sub, self.imageR_sub], queue_size=2, slop=0.1
             )
-            self.sync_stereo.registerCallback(self.receive_frame)
+            self.sync_stereo.registerCallback(self.receive_stereo_frame)
+        elif imageL_topic is not None:
+            self.imageL_sub = self.create_subscription(
+                CompressedImage, imageL_topic + "/compressed",
+                callback=self.receive_compact_frame, qos_profile=1
+            )
 
         self.pose_sub   = self.create_subscription(
             PoseStamped, pose_topic,
@@ -47,11 +52,17 @@ class MACVO_visualizer_Node(Node):
                 callback=self.receive_map, qos_profile=1
             )
     
-    def receive_frame(self, msg_L: Image, msg_R: Image) -> None:
+    def receive_compact_frame(self, msg: Image) -> None:
+        rr.set_time_nanos(self.TIMELINE, Time.from_msg(msg.header.stamp).nanoseconds)
+        image = from_compressed_image(msg)[..., :3][:, :, ::-1].astype(np.uint8)
+        self.get_logger().info(f"Receive: {image.shape}")
+        rr.log("/world/drone/cam/imgL", rr.Image(image), static=True)
+    
+    def receive_stereo_frame(self, msg_L: Image, msg_R: Image) -> None:
         rr.set_time_nanos(self.TIMELINE, Time.from_msg(msg_L.header.stamp).nanoseconds)
         
-        imageL = from_image(msg_L)[..., :3][..., ::-1]
-        imageR = from_image(msg_R)[..., :3][..., ::-1]
+        imageL = from_image(msg_L)[..., :3][:, :, ::-1].astype(np.uint8)
+        imageR = from_image(msg_R)[..., :3][:, :, ::-1].astype(np.uint8)
         self.get_logger().info(f"Receive: Left={imageL.shape}, Right={imageR.shape}")
 
         rr.log("/world/drone/cam/imgL", rr.Image(imageL), static=True)
@@ -67,7 +78,7 @@ class MACVO_visualizer_Node(Node):
             rotation=rr.datatypes.Quaternion(xyzw=pp_pose.rotation().squeeze().numpy()),
             axis_length=1.0
         ))
-        rr.log("/world/trajectory", rr.LineStrips3D([self.prev_position.numpy().tolist(), pp_pose.translation().numpy().list()]))
+        rr.log("/world/trajectory", rr.LineStrips3D([self.prev_position.numpy().tolist(), pp_pose.translation().numpy().tolist()]))
         self.prev_position = pp_pose.translation()
 
     def receive_map(self, map: PointCloud) -> None:
@@ -80,10 +91,9 @@ def main():
     rclpy.init()
     
     node = MACVO_visualizer_Node(
-        image_topic=(
-            "/zed/zed_node/rgb/image_rect_color",
-            "/zed/zed_node/right/image_rect_color"
-        ),
+        imageL_topic="/zed/zed_node/left/image_rect_color",
+        imageR_topic=None,
+        # imageR_topic="/zed/zed_node/right/image_rect_color",
         pose_topic="/macvo/pose",
         map_topic ="/macvo/map"
     )
