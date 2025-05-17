7b6abf65225bef4bd633d7098e752eab2d983188
Yutian Chen <yutianch@andrew.cmu.edu>
2025-05-16 21:50:46 -0400

    Fix coloring bug

--- macvo_vis/cross_device.py
+++ macvo_vis/cross_device.py
@@ -17,11 +17,11 @@ class MACVO_visualizer_Node(Node):
     NODENAME = "macvo_visualizer"
     TIMELINE = "ros_time_ns"
     
-    def __init__(self, image_topic: tuple[str, str] | tuple[str, None] | None, pose_topic: str, map_topic: str | None = None):
+    def __init__(self, image_topic: tuple[str, str] | tuple[str, None] | None, pose_topic: str, map_topic: str | None = None, track_topic: str | None = None):
         super().__init__(self.NODENAME)
         rr.init(self.NODENAME)
         rr.connect_tcp()
-        rr.set_time_sequence(self.TIMELINE, 0)
+        # rr.set_time_sequence(self.TIMELINE, 0)
         rr.log("/log", rr.TextLog("Rerun session initialized"))
         rr.log("/", rr.ViewCoordinates(xyz=rr.ViewCoordinates.FRD), static=True)
         
@@ -53,32 +53,37 @@ class MACVO_visualizer_Node(Node):
         if map_topic is not None:
             self.map_sub = self.create_subscription(
                 PointCloud, map_topic,
-                callback=self.receive_map, qos_profile=1
+                callback=self.receive_map_constructor("/world/map"), qos_profile=1
+            )
+        
+        if track_topic is not None:
+            self.track_sub = self.create_subscription(
+                PointCloud, track_topic, callback=self.receive_map_constructor("/world/vo"), qos_profile=1
             )
     
     def receive_mono(self, msg_L: Image) -> None:
-        rr.set_time_nanos(self.TIMELINE, Time.from_msg(msg_L.header.stamp).nanoseconds)
+        # rr.set_time_nanos(self.TIMELINE, Time.from_msg(msg_L.header.stamp).nanoseconds)
         
         imageL = from_image(msg_L)[..., :3][..., ::-1]
         self.get_logger().info(f"Receive: Left={imageL.shape}, Right=None")
 
-        rr.log("/world/drone/cam/imgL", rr.Image(imageL).compress(), static=True)
+        rr.log("/world/drone/cam/imgL", rr.Image(imageL).compress(), static=False)
     
     def receive_frame(self, msg_L: Image, msg_R: Image) -> None:
-        rr.set_time_nanos(self.TIMELINE, Time.from_msg(msg_L.header.stamp).nanoseconds)
+        # rr.set_time_nanos(self.TIMELINE, Time.from_msg(msg_L.header.stamp).nanoseconds)
         
         imageL = from_image(msg_L)[..., :3][..., ::-1]
         imageR = from_image(msg_R)[..., :3][..., ::-1]
         self.get_logger().info(f"Receive: Left={imageL.shape}, Right={imageR.shape}")
 
-        rr.log("/world/drone/cam/imgL", rr.Image(imageL), static=True)
-        rr.log("/world/drone/cam/imgR", rr.Image(imageR), static=True)
+        rr.log("/world/drone/cam/imgL", rr.Image(imageL), static=False)
+        rr.log("/world/drone/cam/imgR", rr.Image(imageR), static=False)
 
     def receive_pose(self, pose: PoseStamped) -> None:
         pp_pose, _, time = from_stamped_pose(pose)
         if self.prev_position is None: self.prev_position = pp_pose.translation()
 
-        rr.set_time_nanos(self.TIMELINE, Time.from_msg(time).nanoseconds)
+        # rr.set_time_nanos(self.TIMELINE, Time.from_msg(time).nanoseconds)
         rr.log("/world/drone/cam/imgL", rr.Transform3D(
             translation=pp_pose.translation().squeeze().numpy(),
             rotation=rr.datatypes.Quaternion(xyzw=pp_pose.rotation().squeeze().numpy()),
@@ -87,10 +92,17 @@ class MACVO_visualizer_Node(Node):
         rr.log("/world/trajectory", rr.LineStrips3D([self.prev_position.numpy().tolist(), pp_pose.translation().numpy().tolist()]))
         self.prev_position = pp_pose.translation()
 
-    def receive_map(self, map: PointCloud) -> None:
-        position, color, _, time = from_pointcloud(map)
-        rr.set_time_nanos(self.TIMELINE, Time.from_msg(time).nanoseconds)
-        rr.log("/world/map", rr.Points3D(position.numpy(), colors=color.int().numpy()))
+    def receive_map_constructor(self, rr_path: str):
+        def receive_map(map: PointCloud) -> None:
+            position, color, _, time = from_pointcloud(map)
+            
+            if color.size(0) < 10:
+                print(color.size)
+                return
+            
+            # rr.set_time_nanos(self.TIMELINE, Time.from_msg(time).nanoseconds)
+            rr.log(rr_path, rr.Points3D(position.numpy(), colors=color.int().numpy()))
+        return receive_map
 
 
 def main():
@@ -102,7 +114,8 @@ def main():
             None
         ),
         pose_topic="/macvo/pose",
-        map_topic ="/macvo/map"
+        map_topic ="/macvo/map",
+        track_topic="/macvo/track"
     )
     print('MACSLAM Visualization Node created.')
     
